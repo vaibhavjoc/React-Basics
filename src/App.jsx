bf61e421141645a172a3855ae2e014d6a531d2e1
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-10-01 23:45:07 +0530

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,97 +5,68 @@ import './App.css'
 
 function App() {
   return (
-    <div style={{ background: "#898e90ff", height: "100vh" }}>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+    <div style={{ display: "flex", justifyContent: "center", backgroundColor: "#7c7f82ff" }}>
+
+      <div>
         <div>
-          <div>
-            <br />
-            <ProfileCard />
-            <br />
-          </div>
-          <div>
-            <PostComponent />
-            <br />
-          </div>
-          <div>
-            <PostComponent />
-            <br />
-          </div>
-          <div>
-            <PostComponent />
-            <br />
-          </div>
+          <PostComponent
+            name={"harkirat"}
+            subtitle={"20 followers"}
+            time={"2m ago"}
+            image={"https:/appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
+            description={"What to know how to win big? Check out how these folks won $6000 in bounties."}
+          />
+          <br />
+        </div>
+        
+        <div>
+          <PostComponent
+            name={"Baljeet"}
+            subtitle={"Promoted"}
+            image={"https://www.toyota.com.kh/content/dam/cambodia/models/pickup/hilux-rally/head/1.jpg"}
+            description={"The Toyota Hilux is a five-seater, four-wheel-drive pickup truck known for its durability, reliability, and off-road capabilities. The India-spec model shares its platform and 2.8L diesel engine with the Toyota Fortuner."}
+          />
+          <br />
         </div>
       </div>
+
     </div>
   );
 }
-function PostComponent() {
-  const style = { width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, margin: 10, padding: 5 };
+
+const PostComponent = ({ image, name, subtitle, time, description }) => {
+  const style = { width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, margin: 10, padding: 5, boxShadow: "5px 5px 5px #2a2a2aff" };
 
   return (
     <div style={style}>
-      <div style={{ display: "flex" }}>
-        <img src="https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" style={{
-          width: 30,
-          height: 30,
-          borderRadius: 20
-        }} />
+      <div style={{ display: "flex", alignItems: "center" }}> {/* Added alignItems: "center" for better alignment */}
+        <img
+          src={image}
+          alt="Profile" // Added alt text for accessibility
+          style={{
+            width: 30,
+            height: 30,
+            borderRadius: 20,
+          }}
+        />
         <div style={{ fontSize: 10, marginLeft: 10 }}>
-          <b>
-            100xDevs
-          </b>
-          <div>
-            23888 followers
-          </div>
-          <div>
-            12m
-          </div>
+          <b>{name}</b>
+          <div>{subtitle}</div>
+          {time !== undefined && <div style={{ display: "flex", alignItems: "center" }}> {/* Added alignItems: "center" for better alignment */}
+            <div>{time}</div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Icons8_flat_clock.svg/1024px-Icons8_flat_clock.svg.png"
+              alt="Clock icon" // Added alt text for accessibility
+              style={{ width: 10, height: 10, marginLeft: 3 }} // Added marginLeft for spacing
+            />
+          </div>}
         </div>
-
       </div>
-      <div style={{ fontSize: 12 }}>
-        Want to know how to win big? Check out how these folks won $6000 in bounties.
+      <div style={{ fontSize: 12, marginTop: 10 }}> {/* Added marginTop for spacing */}
+        {description}
       </div>
     </div>
   );
-}
-
-function ProfileCard() {
-
-  const style = { width: 200,height: 250, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, };
-
-  return (
-    <div style={style}>
-      <div style={{ background: "#759aa9ff", backgroundSize: 200, height: 65, borderRadius: 8}}>
-        <br />
-        <br />
-        <div style={{ textAlign: "center" }}>
-          <img src="https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" style={{
-            width: 60,
-            height: 60,
-            borderRadius: 100
-          }} />
-        </div>
-        <br />
-        <div style={{ textAlign: "center" }}>
-          Harkirat Singh
-        </div>
-        <div style={{ textAlign: "center" }}>
-          <p style={{ fontSize: 11, color: "#94989aff" }}>Working with WebRTC</p>
-        </div>
-        <hr />
-        <div style={{display: "flex"}}>
-          <p style={{ fontSize: 11, color: "#5e6162ff", marginRight: 90, paddingLeft: 10}}>Profile Views</p>
-          <p style={{ fontSize: 11, color: "#2159d0ff"}}>41903</p>
-        </div>
-        <div style={{display: "flex"}}>
-          <p style={{ fontSize: 11, color: "#5e6162ff", marginRight: 80, paddingLeft: 10}}>Post Impressions</p>
-          <p style={{ fontSize: 11, color: "#2159d0ff"}}>1313</p>
-        </div>
-      </div>
-    </div>
-  )
-}
+};
 
 export default App
