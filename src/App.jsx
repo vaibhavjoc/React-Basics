ad9c6156cb51ef6f6dc01f88630ca6cb1dc89b12
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-10-03 23:29:00 +0530

    Todo rendering

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,42 @@ import { PostComponent } from "./Post";
 
 function App() {
 
-  const [currentTAb, setCurrentTAb] = useState("feed")
+  const [currentTAb, setCurrentTAb] = useState(1);
+  const [tabData, setTabData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function(){
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/todos/" + currentTAb)
+    .then(async res => {
+      const json = await res.json();
+      setTabData(json)
+
+      setLoading(false);
+    })
+  }, [currentTAb])
 
   return (
     <div>
       
-      <button onClick={function () { setCurrentTAb("feed") }} style={{
-        color: currentTAb == "feed" ? "red" : "black"
-      }} >Feed</button>
+      <button onClick={function () { setCurrentTAb(1) }} style={{
+        color: currentTAb == 1 ? "red" : "black"
+      }} >Todo #1</button>
 
-      <button onClick={() => { setCurrentTAb("Notifications") }} style={{
-        color: currentTAb == "Notifications" ? "red" : "black"
-      }} >Notifications</button>
+      <button onClick={() => { setCurrentTAb(2) }} style={{
+        color: currentTAb == 2 ? "red" : "black"
+      }} >Todo #2</button>
 
-      <button onClick={() => { setCurrentTAb("jobs")  }} style={{
-        color: currentTAb == "jobs" ? "red" : "black"
-      }} >jobs</button>
+      <button onClick={() => { setCurrentTAb(3)  }} style={{
+        color: currentTAb == 3 ? "red" : "black"
+      }} >Todo #3</button>
 
-      <button onClick={() => { setCurrentTAb("messages") }} style={{
-        color: currentTAb == "messages" ? "red" : "black"
-      }} >messages</button>
+      <button onClick={() => { setCurrentTAb(4) }} style={{
+        color: currentTAb == 4 ? "red" : "black"
+      }} >Todo #4</button>
+
+      <br />
+      {loading ? "loading....." : tabData.title}
 
     </div>
   );
