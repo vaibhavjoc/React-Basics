9f7ac4b74f604ecdab2e7eae820fcc6b716e3917
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-10-11 00:20:53 +0530

    stopwatch using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,48 @@ import { useState } from 'react'
 
 function App() {
 
-  const [count, setCount] = useState(0);
-  const [counts, setCounts] = useState(0);
+  const [countSeconds, setCountSeconds] = useState(0);
+  const [countMiliseconds, setCountsMiliseconds] = useState(0);
 
   //here we have used useRef to create a reference to a value
   //since it does not re-renders when updated
-  const timer = useRef()
-  const timers = useRef()
+  const timerSeconds = useRef()
+  const timerMiliseconds = useRef()
 
   function startTimer() {
     let value = setInterval(() => {
-      setCount(c => c + 1)
+      setCountSeconds(c => c + 1)
     }, 1000);
+
     let values = setInterval(() => {
-      setCounts(c => c + 1)
+      setCountsMiliseconds(c => c + 1)
     }, 10);
 
-    // we have assigned the current setInterval to timer
+    // we have assigned the current setInterval to timerSeconds
     // so that we can clear it with function stopTimer
-    timer.current = value
-    timers.current = values
+    timerSeconds.current = value
+    timerMiliseconds.current = values
+
+  }
+
+  //ended the 100 milliseconds and starts it again from 0
+  if (counts > 99) {
+    clearInterval(timerMiliseconds.current)
+    setCounts(c => c = 0)
+    let values = setInterval(() => {
+      setCountsMiliseconds(c => c + 1)
+    }, 10);
+    timerMiliseconds.current = values
   }
 
   function stopTimer() {
-    clearInterval(timer.current)
-    clearInterval(timers.current)
+    clearInterval(timerSeconds.current)
+    clearInterval(timerMiliseconds.current)
   }
 
-  return(
+  return (
     <div>
-      <h1>{count}:{counts}</h1>
+      <h1>{countSeconds}:{countMiliseconds}</h1>
       <button onClick={startTimer}>Start</button>
       <button onClick={stopTimer}>Stop</button>
     </div>
