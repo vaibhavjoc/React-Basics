d8cfb2b9ff12a73aed0f05f36db6323104e17fb3
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-10-12 15:37:39 +0530

    Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,64 @@
-import { useState } from 'react'
+import { useState, createContext, useContext } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//remove the hassle of doing prop drilling
+//First we have to create context (should be global) (using createContext)
+//then provide the value to context (using Provider)
+//finally consume it (using useContext)
+//it makes the code look clean
+
+const BulbContext = createContext()
+
 function App() {
-  
-  return(
+
+  const [bulbOn, setBulbOn] = useState(true)
+
+  return (
     <>
-    <LightBulb />
+      <BulbContext.Provider value={{
+        bulbOn: bulbOn,
+        setBulbOn: setBulbOn
+      }}>
+        <LightBulb />
+      </BulbContext.Provider>
     </>
   )
 
 }
 
 function LightBulb() {
-  const [bulbOn, setBulbOn] = useState(true)
+
 
   return <>
-    <BulbState bulbOn={bulbOn} />
-    <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+    <ToggleBulbState />
+    <BulbState />
   </>
 }
 
-function BulbState({bulbOn}) {
+function BulbState() {
+
+  const { bulbOn } = useContext(BulbContext)
+
   return (
-    <>
-    {bulbOn ? <img height={250} src='https://i.pinimg.com/564x/e0/fd/25/e0fd25f9127a9a109a0648c83ee61643.jpg' /> : <img height={230} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnF3A4ktwNpeii6GcrDsKZSkc1l97hochG_Q&s' /> }
-    </>
+    <div>
+      {bulbOn ? <img height={250} src='https://i.pinimg.com/564x/e0/fd/25/e0fd25f9127a9a109a0648c83ee61643.jpg' /> : <img height={230} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnF3A4ktwNpeii6GcrDsKZSkc1l97hochG_Q&s' />}
+    </div>
   )
 }
 
-function ToggleBulbState({bulbOn, setBulbOn}) {
+function ToggleBulbState() {
+
+  const { bulbOn, setBulbOn } = useContext(BulbContext)
 
   function toggle() {
     // setBulbOn(currentState => !currentState)
     setBulbOn(!bulbOn)
-    
+
   }
 
   return <>
-  <br />
     <button onClick={toggle}>Toggle the bulb</button>
   </>
 }
