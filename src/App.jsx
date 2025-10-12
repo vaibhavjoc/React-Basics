bf6b752fc3e522e9658b892fd8c473f820d643cc
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-10-12 15:19:34 +0530

    Rolling Up The state

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,47 @@
-import React, { useRef } from 'react';
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-
-  const [countSeconds, setCountSeconds] = useState(0);
-  const [countMiliseconds, setCountsMiliseconds] = useState(0);
-
-  //here we have used useRef to create a reference to a value
-  //since it does not re-renders when updated
-  const timerSeconds = useRef()
-  const timerMiliseconds = useRef()
-
-  function startTimer() {
-    let value = setInterval(() => {
-      setCountSeconds(c => c + 1)
-    }, 1000);
-
-    let values = setInterval(() => {
-      setCountsMiliseconds(c => c + 1)
-    }, 10);
-
-    // we have assigned the current setInterval to timerSeconds
-    // so that we can clear it with function stopTimer
-    timerSeconds.current = value
-    timerMiliseconds.current = values
-
-  }
-
-  //ended the 100 milliseconds and starts it again from 0
-  if (counts > 99) {
-    clearInterval(timerMiliseconds.current)
-    setCounts(c => c = 0)
-    let values = setInterval(() => {
-      setCountsMiliseconds(c => c + 1)
-    }, 10);
-    timerMiliseconds.current = values
-  }
-
-  function stopTimer() {
-    clearInterval(timerSeconds.current)
-    clearInterval(timerMiliseconds.current)
-  }
-
-  return (
-    <div>
-      <h1>{countSeconds}:{countMiliseconds}</h1>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
-    </div>
+  
+  return(
+    <>
+    <LightBulb />
+    </>
   )
 
+}
+
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(true)
+
+  return <>
+    <BulbState bulbOn={bulbOn} />
+    <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+  </>
+}
+
+function BulbState({bulbOn}) {
+  return (
+    <>
+    {bulbOn ? <img height={250} src='https://i.pinimg.com/564x/e0/fd/25/e0fd25f9127a9a109a0648c83ee61643.jpg' /> : <img height={230} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnF3A4ktwNpeii6GcrDsKZSkc1l97hochG_Q&s' /> }
+    </>
+  )
+}
+
+function ToggleBulbState({bulbOn, setBulbOn}) {
+
+  function toggle() {
+    // setBulbOn(currentState => !currentState)
+    setBulbOn(!bulbOn)
+    
+  }
+
+  return <>
+  <br />
+    <button onClick={toggle}>Toggle the bulb</button>
+  </>
 }
 
 export default App
