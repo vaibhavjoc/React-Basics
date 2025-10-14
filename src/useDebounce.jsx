b7f59df79964c246a2d1d253bc045495fd2b5f32
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-10-14 21:42:10 +0530

    new useDebounced hook

--- src/useDebounce.jsx
+++ src/useDebounce.jsx
@@ -1,28 +1,68 @@
 import { useState, useEffect, useRef } from 'react'
 
-function useDebounce(originalFn){
-    let currentClock = useRef()
+// function useDebounce(originalFn){
+//     let currentClock = useRef()
 
-    const fn = () => {
-        clearTimeout(currentClock.current)
-        currentClock.current = setTimeout(originalFn, 200)
-    }
+//     const fn = () => {
+//         clearTimeout(currentClock.current)
+//         currentClock.current = setTimeout(originalFn, 200)
+//     }
 
-    return fn
+//     return fn
+// }
+
+// function App() {
+//     function sendDataToBackend(){
+//         fetch("amazon.com/search")
+//     }
+
+//     const debouncedFn = useDebounce(sendDataToBackend);
+
+//     return (
+//         <>
+//         <input type="text" onChange={debouncedFn} />
+//         </>
+//     )
+// }
+
+
+//a better approach using useState hook
+
+const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value, delay])
+
+    return debouncedValue
 }
 
 function App() {
-    function sendDataToBackend(){
-        fetch("amazon.com/search")
+    const [inputVal, setInputVal] = useState("")
+    const debouncedVal = useDebounce(inputVal, 200)
+
+    function change(e) {
+        setInputVal(e.target.value)
     }
 
-    const debouncedFn = useDebounce(sendDataToBackend);
+    useEffect(() => {
+        console.log("Hello Mitro")
+    }, [debouncedVal])
 
     return (
         <>
-        <input type="text" onChange={debouncedFn} />
+            <input type="text" onChange={change} />
         </>
     )
+
 }
 
-export default App
+
+export default App;
